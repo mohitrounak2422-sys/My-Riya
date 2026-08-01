1b43244295ae9e6c348a8221c326074092cca500
mohitrounak2422-sys <mohitrounak2422@gmail.com>
2026-08-01 19:06:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-document.getElementById("openBtn").addEventListener("click", function () {
-    alert("Welcome My Princess ❤️\n\nThis is only the beginning...");
-});
+const hearts = document.querySelector(".floating-hearts");
+
+function createHeart(){
+
+    const heart = document.createElement("span");
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random()*100 + "%";
+
+    heart.style.fontSize = (20 + Math.random()*35) + "px";
+
+    heart.style.animationDuration = (5 + Math.random()*5) + "s";
+
+    hearts.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },10000);
+
+}
+
+setInterval(createHeart,300);
+
+
+document.getElementById("openBtn").onclick=function(){
+
+    alert("Welcome My Princess ❤️");
+
+}
